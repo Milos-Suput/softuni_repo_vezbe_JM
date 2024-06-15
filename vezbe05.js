6a72ce73e3389b875dc7e1b741d9f0f5ea2429c4
Milos-Suput <ms.milos.suput@gmail.com>
2024-06-15 12:52:16 +0200

    komit

---
+++ vezbe05.js
@@ -0,0 +1,8 @@
+function poundsToDollars(pound){
+
+    dollar = pound * 1.31
+
+    console.log((dollar).toFixed(3))
+}
+
+poundsToDollars(39)
---
+++ vezbe06.js
@@ -0,0 +1,6 @@
+function reversedChars(input1, input2, input3){
+    console.log(input3, input2, input1)
+
+}
+
+reversedChars('A', 'B', 'C')
